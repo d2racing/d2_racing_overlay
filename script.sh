bdad91df9748eaff1a8e3b045b48f439269c79e2
Sylvain Alain <d2racing911@gmail.com>
2012-01-25 22:08:54 -0500

    test 4

--- script.sh
+++ script.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 ln -fn /etc/portage/package.use/audacious /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/audacious
+
+ln -fn /etc/portage/package.use/conky /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/conky
+
+ln -fn /etc/portage/package.use/cups /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/cups
+
+ln -fn /etc/portage/package.use/git /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/git
+
+ln -fn /etc/portage/package.use/hal /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/hal
+
+ln -fn /etc/portage/package.use/iproute2 /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/iproute2
+
+ln -fn /etc/portage/package.use/libpng /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/libpng
+
+ln -fn /etc/portage/package.use/libxml2 /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/libxml2
+
+ln -fn /etc/portage/package.use/ntfs /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/ntfs
+
+ln -fn /etc/portage/package.use/openssh /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/openssh
+
+ln -fn /etc/portage/package.use/rxvt-unicode /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/rxvt-unicode
+
+ln -fn /etc/portage/package.use/subversion /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/subversion
+
+ln -fn /etc/portage/package.use/transmission /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/transmission
+
+ln -fn /etc/portage/package.use/virtualbox /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/virtualbox
+
+ln -fn /etc/portage/package.use/vlc /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/vlc
+
+ln -fn /etc/portage/package.use/zlib /home/sylvain/local/portage/d2_racing_overlay/Funbox/package.use/zlib
